a1a5ce2d32e41f24a767ebbd2b5f5207cd112867
murilo-toddy <murilo.todao@usp.br>
2022-01-12 15:22:08 -0300

    Extracts handler module to command insertion

---
+++ commands/handler.py
@@ -0,0 +1,12 @@
+from telegram.ext import CommandHandler
+import commands.subsystems as subsystems
+import commands.general as general
+
+def log_command(cmd: str): print(f"[!!] Command {cmd} called")
+
+def register_commands(dsp):
+    dsp.add_handler(CommandHandler("bt", subsystems.bt))
+    dsp.add_handler(CommandHandler("pt", subsystems.pt))
+    dsp.add_handler(CommandHandler("hw", subsystems.hw))
+    dsp.add_handler(CommandHandler("sw", subsystems.sw))
+    dsp.add_handler(CommandHandler("planilha", general.send_sheet))
