98c868553ef9bdf17e208faa79b10352d5e85e20
murilo-toddy <murilo.todao@usp.br>
2022-01-12 12:38:07 -0300

    Creates utils file to list subsystems information

---
+++ utils.py
@@ -0,0 +1,18 @@
+subsystems = {
+    "bt": {
+        "name": "Baterias",
+        "sheet_id": 1546038790
+    },
+    "pt": {
+        "name": "Powertrain",
+        "sheet_id": 1047261973
+    },
+    "hw": {
+        "name": "Hardware",
+        "sheet_id": 0
+    },
+    "sw": {
+        "name": "Software",
+        "sheet_id": 447316715
+    }
+}
