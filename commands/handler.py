991954ed295895b8f6a66683e1230ca234636e20
murilo-toddy <murilo.todao@usp.br>
2022-01-12 17:46:13 -0300

    Implements buttons to generic message

--- commands/handler.py
+++ commands/handler.py
@@ -1,4 +1,5 @@
 from telegram.ext import CommandHandler
+from telegram.ext.callbackqueryhandler import CallbackQueryHandler
 import commands.subsystems as subsystems
 import commands.general as general
 
@@ -9,4 +10,7 @@ def register_commands(dsp):
     dsp.add_handler(CommandHandler("pt", subsystems.pt))
     dsp.add_handler(CommandHandler("hw", subsystems.hw))
     dsp.add_handler(CommandHandler("sw", subsystems.sw))
+    
+    dsp.add_handler(CallbackQueryHandler(subsystems.query_handler))
+    
     dsp.add_handler(CommandHandler("planilha", general.send_sheet))
