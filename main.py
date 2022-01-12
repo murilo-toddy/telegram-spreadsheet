f3929ca8a9c174a715c08cd15fa12e035f839ecc
murilo-toddy <murilo.todao@usp.br>
2022-01-12 11:41:48 -0300

    Clears up main file

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from config import bot, ss
-from commands import *
-
-print("[!] Bot loaded")
-bot.polling()
-# while True:
-#     try:
-#         bot.polling(none_stop=True)
-#     except Exception as e:
-#         print(e)
-#         time.sleep(5)
-
+from bot import bot
 
 if __name__ == "__main__":
-    bot.polling()
+    print("[!] Bot started")
+    bot.start_polling()
+    bot.idle()
