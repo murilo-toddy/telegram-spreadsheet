bb3b82f79968e0dfd7fdf61960e5341f109770c2
murilo-toddy <murilo.todao@usp.br>
2022-01-12 21:17:42 -0300

    Implements button callback handlers

--- commands/handler.py
+++ commands/handler.py
@@ -1,16 +1,17 @@
 from telegram.ext import CommandHandler
 from telegram.ext.callbackqueryhandler import CallbackQueryHandler
-import commands.subsystems as subsystems
+import commands.subsystems.generic as generic
 import commands.general as general
 
 def log_command(cmd: str): print(f"[!!] Command {cmd} called")
 
 def register_commands(dsp):
-    dsp.add_handler(CommandHandler("bt", subsystems.bt))
-    dsp.add_handler(CommandHandler("pt", subsystems.pt))
-    dsp.add_handler(CommandHandler("hw", subsystems.hw))
-    dsp.add_handler(CommandHandler("sw", subsystems.sw))
+    dsp.add_handler(CommandHandler("bt", generic.bt))
+    dsp.add_handler(CommandHandler("pt", generic.pt))
+    dsp.add_handler(CommandHandler("hw", generic.hw))
+    dsp.add_handler(CommandHandler("sw", generic.sw))
     
-    dsp.add_handler(CallbackQueryHandler(subsystems.query_handler))
+    dsp.add_handler(CallbackQueryHandler(generic.query_handler))
+    dsp.add_handler(CallbackQueryHandler(general.send_sheet))
     
     dsp.add_handler(CommandHandler("planilha", general.send_sheet))
