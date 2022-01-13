757e52a1fad2ebc02262bd45154465ef1015ce63
murilo-toddy <murilo.todao@usp.br>
2022-01-12 21:17:23 -0300

    Updates send message method

--- commands/general.py
+++ commands/general.py
@@ -2,6 +2,6 @@ from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
 from spreadsheet import SHEET_URL
 
-def send_sheet(update: Update, ctx: CallbackContext):
-    update.message.reply_text(f'<a href="{SHEET_URL}">Planilha de Planejamento</a>',
+def send_sheet(update: Update, ctx: CallbackContext) -> None:
+    ctx.bot.send_message(chat_id=update.effective_chat.id, text=f'<a href="{SHEET_URL}">Planilha de Planejamento</a>',
                               parse_mode=ParseMode.HTML)
