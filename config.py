3f3c96ec4e00257807cfb0954d934ca7ef348eb1
murilo-toddy <murilo.todao@usp.br>
2022-01-17 18:17:45 -0300

    Extracts env variable loading

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+if os.path.isfile("./.env"): 
+    load_dotenv()
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    COMMANDS_SHEET_ID = os.getenv("COMMANDS_SHEET_ID")
+    ELE_SHEET_ID = os.getenv("ELE_SHEET_ID")
+
+else:
+    TELEGRAM_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+    COMMANDS_SHEET_ID = os.environ["COMMANDS_SHEET_ID"]
+    ELE_SHEET_ID = os.environ["ELE_SHEET_ID"]
