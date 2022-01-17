50f238a081d394db6324746ef95840a1c7e1f71a
murilo-toddy <murilo.todao@usp.br>
2022-01-17 18:17:31 -0300

    Implements sheet connection by ID

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,20 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from utils import subsystems
+from config import COMMANDS_SHEET_ID, ELE_SHEET_ID
 
 SHEET_AUTH_FILE = 'client_secret.json'
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1kHx2-Q3H_m3pqm7YHDmMD8SzOJMxPIIw5cwB7vVlMi0/edit#gid=1546038790"
 SHEET_SCOPE = [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.file'
-    ]
-
-COMMANDS_SHEET_NAME = "Bot Commands"
-ELE_SHEET_NAME = "Planejamento T-06 Prime"
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+]
 
 
 class Spreadsheet:
-    def __init__(self, sheet_name: str, scope: list, auth_file: str, debug: bool):
+    def __init__(self, sheet_id: str, scope: list, auth_file: str, debug: bool):
         self.__debug = debug
         
         # Autenticação na Google API
@@ -22,7 +20,7 @@ class Spreadsheet:
         self.client = gspread.authorize(self.creds)
         
         # Abertura da planilha
-        self.ss = self.client.open(sheet_name)
+        self.ss = self.client.open_by_key(sheet_id)
         self.sheets = {}
 
         if self.__debug: print("[!] Connected do spreadsheet")
@@ -38,10 +36,10 @@ class Spreadsheet:
 
 
 # Planilha de Comandos
-commands: Spreadsheet = Spreadsheet(COMMANDS_SHEET_NAME, SHEET_SCOPE, SHEET_AUTH_FILE, True)
+commands: Spreadsheet = Spreadsheet(COMMANDS_SHEET_ID, SHEET_SCOPE, SHEET_AUTH_FILE, True)
 commands.add_sheet("cmd", 0)
 
 # Planilha da Elétrica
-ss: Spreadsheet = Spreadsheet(ELE_SHEET_NAME, SHEET_SCOPE, SHEET_AUTH_FILE, True)
+ss: Spreadsheet = Spreadsheet(ELE_SHEET_ID, SHEET_SCOPE, SHEET_AUTH_FILE, True)
 for subsystem, info in subsystems.items(): 
     ss.add_sheet(subsystem, info["sheet_id"])
