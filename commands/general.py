87dbb218d472284842b5a5ca3dfa818942e5c03d
murilo-toddy <murilo.todao@usp.br>
2022-01-17 16:33:58 -0300

    Implements generic spreadsheet text response

--- commands/general.py
+++ commands/general.py
@@ -1,7 +1,23 @@
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
-from spreadsheet import SHEET_URL
+from spreadsheet import commands, SHEET_URL
 
 def send_sheet(update: Update, ctx: CallbackContext) -> None:
-    ctx.bot.send_message(chat_id=update.effective_chat.id, text=f'<a href="{SHEET_URL}">Planilha de Planejamento</a>',
-                              parse_mode=ParseMode.HTML)
+    ctx.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=f'<a href="{SHEET_URL}">Planilha de Planejamento</a>',
+        parse_mode=ParseMode.HTML
+    )
+
+
+def spreadsheet_return_text(update: Update, ctx: CallbackContext) -> None:
+    cmd = update.message.text[1:]
+    l = [ cmds[0] for cmds in commands.sheet("cmd").get_all_values()[1:] ]
+    print(f"[!!] Command {cmd} called")
+    
+    index = l.index(cmd) + 1
+    ctx.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=commands.sheet("cmd").get_all_values()[index][1],
+        parse_mode=ParseMode.HTML
+    )
