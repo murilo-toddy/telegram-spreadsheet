a4f9ea9a8789b4a59f745c1398fd4bdf3bd8a5b7
murilo-toddy <murilo.todao@usp.br>
2022-01-18 20:17:14 -0300

    Starts task adding implementation

--- commands/subsystems/task_register.py
+++ commands/subsystems/task_register.py
@@ -1,4 +1,10 @@
-from telegram import Update, ReplyKeyboardMarkup, ParseMode
+from typing import NewType
+from telegram import (
+    Update, 
+    ReplyKeyboardMarkup, 
+    ReplyKeyboardRemove,
+    ParseMode
+)
 from telegram.ext import (
     MessageHandler,
     Filters,
@@ -6,10 +12,15 @@ from telegram.ext import (
     CallbackContext, 
     ConversationHandler
 )
+from utils import ele_subsystems, mec_subsystems
+from spreadsheet import ele_ss, mec_ss
+import commands.general as general
 
-SUBSYSTEM, PROJECT, TASK, DIFFICULTY, DURATION, DOCUMENTS = range(6)
 
-new_task = {
+SYSTEM, SUBSYSTEM, PROJECT, TASK, DIFFICULTY, DURATION, DOCUMENTS = range(7)
+
+task = {
+    "system": "",
     "subsystem": "",
     "project": "",
     "task": "",
@@ -17,13 +28,41 @@ new_task = {
     "duration": "",
     "docs": "",
 }
+new_task = {
+    "ss": None,
+    "dict": None,
+    "task": task
+}
 
 def add_task(update: Update, ctx: CallbackContext):
-    subsystem_selector = [[ 'bt', 'pt' ], [ 'hw', 'sw' ]]
+    system_selector = [[ "ele", "mec" ]]
     update.message.reply_text(
         "<b>Adicionar tarefa</b>\n"
-        "Utilize <code>cancel</code> para cancelar a operação\n\n"
-        "Informe o subsistema\n",
+        "Utilize <code>/cancel</code> a qualquer momento para cancelar a operação\n\n"
+        "Informe o sistema\n",
+        reply_markup=ReplyKeyboardMarkup(
+            system_selector, one_time_keyboard=True
+        ),
+        parse_mode=ParseMode.HTML
+    )
+    return SYSTEM
+
+
+def system(update: Update, ctx: CallbackContext):
+    system = update.message.text
+    if system == "ele":   subsystem_selector = [[ "bt", "pt" ], [ "hw", "sw" ]]
+    elif system == "mec": subsystem_selector = [[ "ch" ]]
+    else:
+        update.message.reply_text("Sistema não encontrado", reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+    
+    global new_task
+    new_task["task"]["system"] = system
+    new_task["dict"] = ele_subsystems if system == "ele" else mec_subsystems
+    new_task["ss"] = ele_ss if system == "ele" else mec_ss
+
+    update.message.reply_text(
+        "Informe o subsistema",
         reply_markup=ReplyKeyboardMarkup(
             subsystem_selector, one_time_keyboard=True
         ),
@@ -32,10 +71,27 @@ def add_task(update: Update, ctx: CallbackContext):
     return SUBSYSTEM
 
 
+def get_active_projects() -> str:
+    global new_task
+    ss = new_task["ss"].sheet(new_task["task"]["subsystem"])
+    print(ss)
+    return 0
+
+
+
 def subsystem(update: Update, ctx: CallbackContext):
-    user = update.message.from_user
-    update.message.reply_text("subsistema")
-    print(f"{user.first_name} selected {update.message.text}")
+    subsystem = update.message.text
+    global new_task
+    new_task["task"]["subsystem"] = subsystem
+    
+    update.message.reply_text(
+        f"<b>Subsistema: {new_task['dict'][subsystem]['name']}</b>\n\n"
+        f"<u>Projetos Ativos</u>\n{get_active_projects()}",
+        reply_markup=ReplyKeyboardRemove(),
+        parse_mode=ParseMode.HTML
+    )
+    
+    print(f"selected {update.message.text}")
     return PROJECT
 
 def project(update: Update, ctx: CallbackContext):
@@ -76,14 +132,15 @@ def cancel(update: Update, ctx: CallbackContext):
 
 
 register_handler = ConversationHandler(
-    entry_points=[CommandHandler('add', add_task)],
-    states={
-        SUBSYSTEM:  [ MessageHandler(Filters.regex('^(bt|pt|hw|sw)$'),  subsystem) ],
-        PROJECT:    [ MessageHandler(Filters.text & ~(Filters.command), project) ],
-        TASK:       [ MessageHandler(Filters.text & ~(Filters.command), task) ],
-        DIFFICULTY: [ MessageHandler(Filters.text & ~(Filters.command), difficulty) ],
-        DURATION:   [ MessageHandler(Filters.text & ~(Filters.command), duration) ],
-        DOCUMENTS:  [ MessageHandler(Filters.text & ~(Filters.command), documents) ], 
-    },
-    fallbacks=[CommandHandler('cancel', cancel)]
-)
+        entry_points=[CommandHandler('add', add_task)],
+        states={
+            SYSTEM:     [ MessageHandler(Filters.text & ~(Filters.command), system)     ],
+            SUBSYSTEM:  [ MessageHandler(Filters.text & ~(Filters.command), subsystem)  ],
+            PROJECT:    [ MessageHandler(Filters.text & ~(Filters.command), project)    ],
+            TASK:       [ MessageHandler(Filters.text & ~(Filters.command), task)       ],
+            DIFFICULTY: [ MessageHandler(Filters.text & ~(Filters.command), difficulty) ],
+            DURATION:   [ MessageHandler(Filters.text & ~(Filters.command), duration)   ],
+            DOCUMENTS:  [ MessageHandler(Filters.text & ~(Filters.command), documents)  ], 
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
