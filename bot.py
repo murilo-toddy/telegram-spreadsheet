683d80b52e022051212c25305cd38397653a17af
murilo-toddy <murilo.todao@usp.br>
2022-01-18 16:58:06 -0300

    Adds comments

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 from telegram.ext import Updater
 from config import TELEGRAM_TOKEN
 import commands.handler as handler
-    
+
+# Creates bot instance
 bot = Updater(TELEGRAM_TOKEN)
 dsp = bot.dispatcher
 
+# Loads commands and handlers
 handler.register_commands(dsp)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bot import bot
 
 if __name__ == "__main__":
     print("[!] Bot started")
+
+    # Starts bot
     bot.start_polling()
     bot.idle()
     
