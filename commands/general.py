5d32f46b0b3e8af756369902ca7ce7fd3d1d4e79
murilo-toddy <murilo.todao@usp.br>
2022-01-18 18:11:29 -0300

    Implements refresh to update spreadsheet functions

--- commands/general.py
+++ commands/general.py
@@ -1,23 +1,27 @@
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
-from spreadsheet import commands, SHEET_URL
+from spreadsheet import commands
+import bot
 
-def send_sheet(update: Update, ctx: CallbackContext) -> None:
+
+def send_message(update: Update, ctx: CallbackContext, text: str) -> None:
     ctx.bot.send_message(
-        chat_id=update.effective_chat.id, 
-        text=f'<a href="{SHEET_URL}">Planilha de Planejamento</a>',
+        chat_id=update.effective_chat.id,
+        text=text,
         parse_mode=ParseMode.HTML
     )
 
 
+# Gets text from commands listed in Bot Commands spreadsheet
 def spreadsheet_return_text(update: Update, ctx: CallbackContext) -> None:
     cmd = update.message.text[1:]
     l = [ cmds[0] for cmds in commands.sheet("cmd").get_all_values()[1:] ]
-    print(f"[!!] Command {cmd} called")
-    
+    print(f"[!!] Command {cmd} called")    
     index = l.index(cmd) + 1
-    ctx.bot.send_message(
-        chat_id=update.effective_chat.id, 
-        text=commands.sheet("cmd").get_all_values()[index][1],
-        parse_mode=ParseMode.HTML
-    )
+    send_message(update, ctx, commands.sheet("cmd").get_all_values()[index][1])
+
+
+# Reloads commands listed in Bot Commands spreadsheet
+def update_sheet_commands(update: Update, ctx: CallbackContext) -> None:
+    bot.handler.register_commands(bot.dsp)
+    send_message(update, ctx, "Comandos atualizados com sucesso!")
