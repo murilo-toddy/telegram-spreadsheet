93db269cc57d4eae57b950575025f062e2b321af
murilo-toddy <murilo.todao@usp.br>
2022-01-18 18:10:57 -0300

    Updates task listing methods for better reading

--- commands/handler.py
+++ commands/handler.py
@@ -1,23 +1,26 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler
+import commands.subsystems.status_updater as status_updater
 import commands.subsystems.task_register as task_register
+import commands.subsystems.task_list as task_list
 import commands.subsystems.generic as generic
 import commands.general as general
 from spreadsheet import commands
 
-def log_command(cmd: str): print(f"[!!] Command {cmd} called")
+def log_command(cmd: str): print(f" [!!] Command {cmd} called")
 
 def register_commands(dsp):
-    dsp.add_handler(CommandHandler("bt", generic.bt))
-    dsp.add_handler(CommandHandler("pt", generic.pt))
-    dsp.add_handler(CommandHandler("hw", generic.hw))
-    dsp.add_handler(CommandHandler("sw", generic.sw))
+    dsp.add_handler(CommandHandler("list", task_list.subsystem_task_lister))
+    dsp.add_handler(CommandHandler("end",  status_updater.conclude_task))
+    dsp.add_handler(CommandHandler("init", status_updater.start_task))
+    dsp.add_handler(CommandHandler("add",  task_register.add_task))
+    
+    dsp.add_handler(CommandHandler("refresh", general.update_sheet_commands))
     
     dsp.add_handler(CallbackQueryHandler(generic.query_handler))
-    dsp.add_handler(CallbackQueryHandler(general.send_sheet))
-    
-    dsp.add_handler(CommandHandler("planilha", general.send_sheet))
-
     dsp.add_handler(task_register.register_handler)
 
     for cmd in commands.sheet("cmd").get_all_values()[1:]:
         dsp.add_handler(CommandHandler(command=cmd[0], callback=general.spreadsheet_return_text))
+
+    print("[!] Commands loaded")
+    
--- commands/subsystems/task_list.py
+++ commands/subsystems/task_list.py
@@ -1,25 +1,92 @@
-from telegram import Update, ParseMode
+from telegram import Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
-from utils import subsystems
-from spreadsheet import ss
+import commands.general as general
+from utils import ele_subsystems, mec_subsystems
+from spreadsheet import ele_ss, mec_ss
+
+def get_subtasks(data, pos: int, counter: int) -> bool:
+    tasks = ""
+    i = pos
+    while i < len(data) and (not data[i][0] or i == pos):
+        if data[i][1] and data[i][2] != "Concluido":
+            tasks += f"{counter} - {data[i][1]}\n"
+            counter += 1
+        i += 1
+    return tasks, i, counter
 
 
-def get_task_lister_text(subsystem: str) -> str:
-    todo_tasks = "\n".join([f"{index+1} - {row[1]}" 
-                            for index, row in enumerate(
-                                row for row in ss.sheet(subsystem).get_all_values()[1:]
-                                if row[2] != "Concluído" and row[1])])
+def get_task_lister_text(system: str, subsystem: str) -> str:
+    if system == "ele":
+        name = ele_subsystems[subsystem]['name']
+        ss   = ele_ss.sheet(subsystem)
+    else:
+        name = mec_subsystems[subsystem]['name']
+        ss   = mec_ss.sheet(subsystem)
+
+    data = ss.get_all_values()
+    string = f"<b>Subsistema: {name}</b>\n\n<u>Tarefas</u>\n"
+    counter = 1
+
+    for i in range(1, len(data)):
+        if data[i][0]:
+            tasks, pos, counter = get_subtasks(data, i, counter)
+            if tasks: string += f"\n<i>{data[i][0]}</i>\n" + tasks
+            i = pos
     
-    return (f"<b>Subsistema: {subsystems[subsystem]['name']}</b>\n\n"
-                "<u>Tarefas:</u>\n"
-                f"{todo_tasks}"
-                ).format(todo_tasks=todo_tasks)
+    return string
 
 
-def subsystem_task_lister(update: Update, ctx: CallbackContext, sub: str, args: str) -> None:
-    if not args:
-        ctx.bot.send_message(chat_id=update.effective_chat.id, text=get_task_lister_text(sub), parse_mode=ParseMode.HTML)
+def task_lister(update: Update, ctx: CallbackContext, args: str) -> None:
+    sub = args[0].strip().lower()
+
+    if sub == "ele":
+        subsystems = [
+            [ InlineKeyboardButton("Baterias",   callback_data="list bt"),
+              InlineKeyboardButton("Powertrain", callback_data="list pt") ],
+            [ InlineKeyboardButton("Hardware",   callback_data="list hw"),
+              InlineKeyboardButton("Software",   callback_data="list sw") ]
+        ]
+        ctx.bot.send_message(chat_id=update.effective_chat.id, 
+            text="<b>Elétrica</b>\n\n"
+                 "O sistema da elétrica possui os seguintes subsistemas:\n"
+                 "- Baterias\n- Powertrain\n- Hardware\n- Software\n\n"
+                 "Escolha o subsistema que deseja listar as tarefas", 
+            reply_markup=InlineKeyboardMarkup(subsystems), parse_mode=ParseMode.HTML
+        )
+
+    elif sub == "mec":
+        subsystems = [
+            [ InlineKeyboardButton("Chassi",   callback_data="list ch") ]
+        ]
+        ctx.bot.send_message(chat_id=update.effective_chat.id, 
+            text="<b>Mecânica</b>\n\n"
+                 "O sistema da mecânica possui os seguintes subsistemas:\n"
+                 "- Chassi\n\n"
+                 "Escolha o subsistema que deseja listar as tarefas",
+            reply_markup=InlineKeyboardMarkup(subsystems), parse_mode=ParseMode.HTML
+        )
+
+    elif sub in ele_subsystems.keys():
+        general.send_message(update, ctx, get_task_lister_text("ele", sub))
+
+    elif sub in mec_subsystems.keys():
+        general.send_message(update, ctx, get_task_lister_text("mec", sub))
 
     else:
-        print(f"Conclude task {args}")
-        
+        general.send_message(update, ctx, "Sistema ou subsistema não encontrado")
+
+
+def subsystem_task_lister(update: Update, ctx: CallbackContext) -> None:
+    args = ctx.args
+    if not args:
+        systems = [
+            [ InlineKeyboardButton("Elétrica", callback_data="list ele"),
+              InlineKeyboardButton("Mecânica", callback_data="list mec") ]
+        ]
+        ctx.bot.send_message(chat_id=update.effective_chat.id, 
+            text="Escolha um sistema ou subsistema\n\n<code>/list &lt;subsistema&gt;</code>", 
+            reply_markup=InlineKeyboardMarkup(systems), parse_mode=ParseMode.HTML
+        )
+            
+    else:
+        task_lister(update, ctx, args)
