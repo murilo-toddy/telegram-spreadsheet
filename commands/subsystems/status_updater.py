a752b0576c50f970ca7029674fc53ce361e7f5f1
murilo-toddy <murilo.todao@usp.br>
2022-01-18 18:12:12 -0300

    Creates status updater file

---
+++ commands/subsystems/status_updater.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+def start_task(update: Update, ctx: CallbackContext) -> None:
+    pass
+
+def conclude_task(update: Update, ctx: CallbackContext) -> None:
+    pass
