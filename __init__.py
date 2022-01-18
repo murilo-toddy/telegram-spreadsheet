f9cfd6452f6c11488574edf402db2410990971cd
murilo-toddy <murilo.todao@usp.br>
2022-01-18 16:58:34 -0300

    Imports modules from commands subfolders

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
-import commands.subsystems as subsystems
+import commands.subsystems.generic as generic
 import commands.handler as handler
 import commands.general as general
+import commands.subsystems.generic as generic
+import commands.subsystems.task_list as task_list
+import commands.subsystems.status_updater as status_updater
