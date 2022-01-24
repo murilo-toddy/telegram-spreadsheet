1daad91f4eec6ad21432adca731e05c54bc89f51
murilo-toddy <murilo.todao@usp.br>
2022-01-24 14:20:14 -0300

    Implement task start handler

--- commands/handler.py
+++ commands/handler.py
@@ -1,8 +1,9 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler
-import commands.subsystems.status_updater as status_updater
+import commands.subsystems.task_conclude as task_conclude
+import commands.subsystems.task_start as task_start
 import commands.subsystems.task_register as task_register
 import commands.subsystems.task_list as task_list
-import commands.subsystems.generic as generic
+import commands.subsystems.query_handler as query_handler
 import commands.general as general
 from spreadsheet import commands
 
@@ -13,18 +14,16 @@ def log_command(cmd: str):
 
 def register_commands(dsp):
     dsp.add_handler(CommandHandler("list", task_list.subsystem_task_lister))
-    dsp.add_handler(CommandHandler("end", status_updater.conclude_task))
-    dsp.add_handler(CommandHandler("init", status_updater.start_task))
+    dsp.add_handler(CommandHandler("end", task_conclude.conclude_task))
 
     dsp.add_handler(CommandHandler("planilha", general.send_sheet))
     dsp.add_handler(CommandHandler("refresh", general.update_sheet_commands))
 
-    dsp.add_handler(CallbackQueryHandler(generic.query_handler))
+    dsp.add_handler(CallbackQueryHandler(query_handler.query_handler))
     dsp.add_handler(task_register.register_handler)
+    dsp.add_handler(task_start.start_handler)
 
     for cmd in commands.sheet("cmd").get_all_values()[1:]:
-        dsp.add_handler(
-            CommandHandler(command=cmd[0], callback=general.spreadsheet_return_text)
-        )
+        dsp.add_handler(CommandHandler(command=cmd[0], callback=general.spreadsheet_return_text))
 
     print("[!] Commands loaded")
