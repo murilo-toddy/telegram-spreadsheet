608b8115e8e63a6da51b578ad26d0fe0bb839a7b
murilo-toddy <murilo.todao@usp.br>
2022-01-24 14:41:43 -0300

    Add conclude_task handler

--- commands/handler.py
+++ commands/handler.py
@@ -14,7 +14,6 @@ def log_command(cmd: str):
 
 def register_commands(dsp):
     dsp.add_handler(CommandHandler("list", task_list.subsystem_task_lister))
-    dsp.add_handler(CommandHandler("end", task_conclude.conclude_task))
 
     dsp.add_handler(CommandHandler("planilha", general.send_sheet))
     dsp.add_handler(CommandHandler("refresh", general.update_sheet_commands))
@@ -22,6 +21,7 @@ def register_commands(dsp):
     dsp.add_handler(CallbackQueryHandler(query_handler.query_handler))
     dsp.add_handler(task_register.register_handler)
     dsp.add_handler(task_start.start_handler)
+    dsp.add_handler(task_conclude.conclude_handler)
 
     for cmd in commands.sheet("cmd").get_all_values()[1:]:
         dsp.add_handler(CommandHandler(command=cmd[0], callback=general.spreadsheet_return_text))
