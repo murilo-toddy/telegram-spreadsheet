02fb5f3e69a87d430e3a7737a75cb0fce5a4ec7e
murilo-toddy <murilo.todao@usp.br>
2022-02-18 17:55:41 -0300

    Fix typo

--- db/connection.py
+++ db/connection.py
@@ -5,5 +5,5 @@ from config import DATABASE_URL
 class Connection:
     def __init__(self):
         print(DATABASE_URL)
-        conn = psycopg2.connection(DATABASE_URL)
+        conn = psycopg2.connect(DATABASE_URL)
         print(conn)
