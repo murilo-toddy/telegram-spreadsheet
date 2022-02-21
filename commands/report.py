e01185a56c4147375b1712b243192e352204bd4d
murilo-toddy <murilo.todao@usp.br>
2022-02-21 16:39:07 -0300

    Get listed report in fix

--- commands/report.py
+++ commands/report.py
@@ -16,8 +16,9 @@ def report_chat_handler(update: Update, ctx: CallbackContext):
         reports = Connection().exec(query, func=lambda cur: cur.fetchall())
         if reports:
             send_message(update, ctx, f"<u>Reports abertos</u> ({len(reports)})")
-            for report in reports:
+            for index, report in enumerate(reports):
                 report_text = (
+                    f"Report {index + 1}\n"
                     f"<b>Comando:</b> {report[1]}\n"
                     f"<b>Corrigido:</b> {'Sim' if report[3] else 'Não'}\n"
                     f"<b>Responsável:</b> @{report[0]}\n"
@@ -26,6 +27,30 @@ def report_chat_handler(update: Update, ctx: CallbackContext):
                 send_message(update, ctx, report_text)
         else:
             send_message(update, ctx, "Não há reports para apresentar")
+
+    elif ctx.args[0] == "fix":
+        query = "SELECT * FROM report WHERE fixed = false;"
+        open_reports = Connection().exec(query, func=lambda cur: cur.fetchall())
+        try:
+            print(len(open_reports))
+            if len(ctx.args) < 2 or len(open_reports) < int(ctx.args[1]) or int(ctx.args[1]) < 1:
+                reply_text = (
+                    f"O report inserido não foi encontrado. Atualmente há {len(open_reports)} abertos.\n"
+                    "Lembre-se de utilizar a sintaxe <code>/report fix [número do report]</code>\n\n"
+                    "Todos os reports podem ser observados através do comando <code>/report list</code>"
+                )
+                send_message(update, ctx, reply_text)
+            else:
+                report = open_reports[int(ctx.args[1]) - 1]
+                print(report)
+
+        except ValueError:
+            reply_text = (
+                f"O report inserido não foi encontrado. Atualmente há {len(open_reports)} abertos.\n"
+                "Lembre-se de utilizar a sintaxe <code>/report fix [número do report]</code>\n\n"
+                "Todos os reports podem ser observados através do comando <code>/report list</code>"
+            )
+            send_message(update, ctx, reply_text)
 
 
 # Command that enables users to report malfunctions
