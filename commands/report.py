a19fde939d53c35f97338fbd7a4a01216790d5f5
murilo-toddy <murilo.todao@usp.br>
2022-02-22 18:29:09 -0300

    Split reports into different methods

--- commands/report.py
+++ commands/report.py
@@ -7,50 +7,58 @@ from .general import send_message, send_message_to
 from .help import available_commands
 
 
+def list_reports(update: Update, ctx: CallbackContext):
+    if len(ctx.args) > 1 and ctx.args[1] == "all":
+        query = "SELECT * FROM report;"
+    else:
+        query = "SELECT * FROM report WHERE fixed = false;"
+    if reports := Connection().exec(query, func=lambda cur: cur.fetchall()):
+        send_message(update, ctx, f"<u>Reports abertos</u> ({len(reports)})")
+        for index, report in enumerate(reports):
+            report_text = (
+                f"Report {index + 1}\n"
+                f"<b>Comando:</b> {report[1]}\n"
+                f"<b>Corrigido:</b> {'Sim' if report[3] else 'Não'}\n"
+                f"<b>Responsável:</b> @{report[0]}\n"
+                f"<b>Problema:</b>\n{report[2]}\n\n"
+                "Deseja corrigir o report?"
+            )
+            send_message(update, ctx, report_text)
+    else:
+        send_message(update, ctx, "Não há reports para apresentar")
+
+
+def fix_report(update: Update, ctx: CallbackContext):
+    query = "SELECT * FROM report WHERE fixed = false;"
+    open_reports = Connection().exec(query, func=lambda cur: cur.fetchall())
+    error_reply = (
+        f"O report inserido não foi encontrado. Atualmente há {len(open_reports)} abertos.\n"
+        "Lembre-se de utilizar a sintaxe <code>/report fix [número do report]</code>\n\n"
+        "Todos os reports podem ser observados através do comando <code>/report list</code>"
+    )
+    try:
+        if len(ctx.args) < 2 or len(open_reports) < int(ctx.args[1]) or int(ctx.args[1]) < 1:
+            send_message(update, ctx, error_reply)
+        else:
+            report = open_reports[int(ctx.args[1]) - 1]
+            report_text = (
+                f"Report {ctx.args[1]}\n"
+                f"<b>Comando:</b> {report[1]}\n"
+                f"<b>Corrigido:</b> {'Sim' if report[3] else 'Não'}\n"
+                f"<b>Responsável:</b> @{report[0]}\n"
+                f"<b>Problema:</b>\n{report[2]}"
+            )
+            print(report_text)
+
+    except ValueError:
+        send_message(update, ctx, error_reply)
+
+
 def report_chat_handler(update: Update, ctx: CallbackContext):
     if not ctx.args or ctx.args[0] == "list":
-        if len(ctx.args) > 1 and ctx.args[1] == "all":
-            query = "SELECT * FROM report;"
-        else:
-            query = "SELECT * FROM report WHERE fixed = false;"
-        reports = Connection().exec(query, func=lambda cur: cur.fetchall())
-        if reports:
-            send_message(update, ctx, f"<u>Reports abertos</u> ({len(reports)})")
-            for index, report in enumerate(reports):
-                report_text = (
-                    f"Report {index + 1}\n"
-                    f"<b>Comando:</b> {report[1]}\n"
-                    f"<b>Corrigido:</b> {'Sim' if report[3] else 'Não'}\n"
-                    f"<b>Responsável:</b> @{report[0]}\n"
-                    f"<b>Problema:</b>\n{report[2]}"
-                )
-                send_message(update, ctx, report_text)
-        else:
-            send_message(update, ctx, "Não há reports para apresentar")
-
+        list_reports(update, ctx)
     elif ctx.args[0] == "fix":
-        query = "SELECT * FROM report WHERE fixed = false;"
-        open_reports = Connection().exec(query, func=lambda cur: cur.fetchall())
-        try:
-            print(len(open_reports))
-            if len(ctx.args) < 2 or len(open_reports) < int(ctx.args[1]) or int(ctx.args[1]) < 1:
-                reply_text = (
-                    f"O report inserido não foi encontrado. Atualmente há {len(open_reports)} abertos.\n"
-                    "Lembre-se de utilizar a sintaxe <code>/report fix [número do report]</code>\n\n"
-                    "Todos os reports podem ser observados através do comando <code>/report list</code>"
-                )
-                send_message(update, ctx, reply_text)
-            else:
-                report = open_reports[int(ctx.args[1]) - 1]
-                print(report)
-
-        except ValueError:
-            reply_text = (
-                f"O report inserido não foi encontrado. Atualmente há {len(open_reports)} abertos.\n"
-                "Lembre-se de utilizar a sintaxe <code>/report fix [número do report]</code>\n\n"
-                "Todos os reports podem ser observados através do comando <code>/report list</code>"
-            )
-            send_message(update, ctx, reply_text)
+        fix_report(update, ctx)
 
 
 # Command that enables users to report malfunctions
